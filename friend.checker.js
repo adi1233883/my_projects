19d7118adefaa3e271148d5ec5406c923b111325
adi1233883 <aditimahajan1912@gmail.com>
2026-05-01 07:09:10 +0000

    yes

---
+++ friend.checker.js
@@ -0,0 +1,13 @@
+let friend_name = prompt("enter your name");
+switch (friend_name.toLowerCase()) {
+    case "ava":
+    case "emma":
+    case "jake":
+    case "mark":
+
+console.log("you are my friend");
+break;
+
+default:
+console.log("who tf are you");
+}
